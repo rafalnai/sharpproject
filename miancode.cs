67f9774207cd8f899f4853440e53c7e5e77e059e
Brad White <161036418+rafalnai@users.noreply.github.com>
2024-02-23 14:54:16 +0200

    Create miancode.cs

---
+++ miancode.cs
@@ -0,0 +1,33 @@
+ public class BoundingFrustum : IEquatable<BoundingFrustum>
+    {
+        #region Private Fields
+
+        private Matrix _matrix;
+        private readonly Vector3[] _corners = new Vector3[CornerCount];
+        private readonly Plane[] _planes = new Plane[PlaneCount];
+
+
+ public Curve Clone()
+        {
+            Curve curve = new Curve();
+
+            curve._keys = this._keys.Clone();
+            curve._preLoop = this._preLoop;
+            curve._postLoop = this._postLoop;
+
+            return curve;
+        }
+
+        
+        public int UpdateOrder
+        {
+            get { return _updateOrder; }
+            set
+            {
+                if (_updateOrder != value)
+                {
+                    _updateOrder = value;
+                    OnUpdateOrderChanged(this, EventArgs.Empty);
+                }
+            }
+        }
